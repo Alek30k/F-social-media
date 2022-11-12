87624d5fd5714a8c48531f293df331d99af675de
Alek30k <alemank23k@gmail.com>
2022-11-11 23:27:27 -0300

    last commit!

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -15,7 +15,7 @@ export default function Feed({ username }) {
         ? await axios.get("/posts/profile/" + username)
         : await axios.get("posts/timeline/" + user._id);
       setPosts(
-        res.data.sort((p1, p2) => {
+        res.data?.sort((p1, p2) => {
           return new Date(p2.createdAt) - new Date(p1.createdAt);
         })
       );
