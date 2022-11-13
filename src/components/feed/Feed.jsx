032cc93dbcfa5ff2974f0e37e26b08541ed053c0
Alek30k <alemank23k@gmail.com>
2022-11-13 16:22:03 -0300

    last commit

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -18,12 +18,12 @@ export default function Feed({ username }) {
         : await axios.get(
             "https://feisbuk-app.herokuapp.com/api/posts/timeline/" + user._id
           );
-      setPosts(res.data);
-      // setPosts(
-      //   res.data.sort((p1, p2) => {
-      //     return new Date(p2.createdAt) - new Date(p1.createdAt);
-      //   })
-      // );
+      // setPosts(res.data);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
     };
     fetchPosts();
   }, [username, user._id]);
